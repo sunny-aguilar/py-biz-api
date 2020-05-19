431bbd12736f246a5dbd0ac576e8877fa2825e3f
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-19 02:42:05 -0700

    client work

--- emails.py
+++ emails.py
@@ -4,5 +4,43 @@ import mimetypes
 import os.path
 import smtplib
 
+def generate_email(sender, recipient, subject, body, attachment_path):
+    """Creates an email with an attachement."""
+    # Basic Email formatting
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
 
+    # Process the attachment and add it to the email
+    attachment_filename = os.path.basename(attachment_path)
+    mime_type, _ = mimetypes.guess_type(attachment_path)
+    mime_type, mime_subtype = mime_type.split('/', 1)
 
+    with open(attachment_path, 'rb') as ap:
+        message.add_attachment(ap.read(),
+                            maintype=mime_type,
+                            subtype=mime_subtype,
+                            filename=attachment_filename)
+
+    return message
+
+def send_email(message):
+    """Sends the message to the configured SMTP server."""
+    mail_server = smtplib.SMTP('localhost')
+    mail_server.send_message(message)
+    mail_server.quit()
+
+def main():
+    sender = 'automation@example.com'
+    recipient = 'student-01-fb6b4f12e8ed@example.com'
+    subject = 'Upload Completed - Online Fruit Store'
+    body = 'All fruits are uploaded to our website successfully. A detailed list is attached to this email.'
+    attachment_path = '/tmp/processed.pdf'
+
+    generate_email(sender, recipient, subject, body, attachment_path)
+    send_email()
+
+if __name__ == "__main__":
+    main()
