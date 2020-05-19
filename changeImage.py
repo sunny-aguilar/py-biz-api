036e88e827c6bf58e2e8a84240f211ceab9ba6e0
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-18 21:03:02 -0700

    client work

--- changeImage.py
+++ changeImage.py
@@ -18,23 +18,29 @@ directory = './supplier-data/images'
 for file in os.listdir(directory):
 
     if file.endswith('.tiff'):
+
+        # file location and name
+        file_loc = './supplier-data/images/'
+
+        # open the file
+        img = Image.open(file_loc + file)
+
+        # resize image
+        newsize = (600, 400)
+        img = img.resize(newsize)
+
+        # save file
+        save_location = './supplier-data/images/' + file + '.jpeg'
+        img.convert('RGB').save(save_location)
+
+
+# iterate over files in directory
+for file in os.listdir(directory):
+
+    if file.endswith('.tiff'):
         # rename file
         new_name = file
         print(f'File: {file}')
         new_name = new_name[0:3] + '.jpeg'
         print(f'New Name: {file}')
         os.rename(directory+'/'+file, directory+'/'+new_name)
-
-        # file location and name
-        file_loc = './supplier-data/images/'
-
-        # open the file
-        # img = Image.open(file_loc + file)
-
-        # resize image
-        # newsize = (600, 400)
-        # img = img.resize(newsize)
-
-        # save file
-        # save_location = './supplier-data/images/' + file + '.jpeg'
-        # img.convert('RGB').save(save_location)
