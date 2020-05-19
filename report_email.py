428f68bdb7715c8b87ce018ebf074354f9bba3d2
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-19 03:13:58 -0700

    client work

--- report_email.py
+++ report_email.py
@@ -58,8 +58,8 @@ def main():
     subject = 'Upload Completed - Online Fruit Store'
     body = 'All fruits are uploaded to our website successfully. A detailed list is attached to this email.'
     attachment_path = '/tmp/processed.pdf'
-    emails.generate_email(sender, recipient, subject, body, attachment_path)
-    emails.send_email()
+    message = emails.generate_email(sender, recipient, subject, body, attachment_path)
+    emails.send_email(message)
 
 
 if __name__ == "__main__":
