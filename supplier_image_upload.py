352a07362de570f268b4245b4e475561215a22fe
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-18 22:15:22 -0700

    client work

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
-import requests
+import requests, os
 
 
 url = "http://localhost/upload/"
+
 with open('/supplier-data/images', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
+    if opened.endswith('.jpeg'):
+        r = requests.post(url, files={'file': opened})
+
+
+# directory to iterate over
+directory = './supplier-data/images'
+
+# iterate over files in directory
+for file in os.listdir(directory):
+    # if file.endswith('.jpeg'):
+    #     r = requests.post(url, files={'file': file})
+    with open('./supplier-data/images/'+file, 'rb') as opened:
+        if file.endswith('.jpeg'):
+            r = requests.post(url, files={'file': opened})
