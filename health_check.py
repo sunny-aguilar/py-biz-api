a4230f1c1f16f110342d7ca9f51c6d5ca38d9e56
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-19 04:48:33 -0700

    client work

---
+++ health_check.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import shutil
+import psutil
+import sched
+import time
+import os
+import sys
+import socket
+
+
+starttime=time.time()
+while True:
+    print("tick")
+    # report an error if CPU usage is over 80%
+    usage = psutil.cpu_percent()
+    if usage > 5:
+        print(usage, '% CPU usage')
+        # send email
+
+    # report an error if available disk space is lower than 20%
+    disk_usage = psutil.disk_usage('./')
+    free_disk_space = 100 - disk_usage[3]
+    if free_disk_space < 20:
+        print(free_disk_space, '%')
+        # send email
+
+    # report an error if available memory is less than 500MB
+    memory = psutil.virtual_memory()
+    mem_avail = memory[1]/1024
+    mem_avail = mem_avail/1024
+    print(mem_avail, 'MB free disk space')
+    if mem_avail < 500:
+        print(mem_avail, 'MB')
+
+    # report an error if the host name 'localhost' cannot be resolved to '127.0.0.1'
+    s = socket.gethostbyname(socket.gethostname())
+    print(s)
+    if s is not '127.0.0.1':
+        print('Unresolved localhost')
+        # send email
+ 
+
+    time.sleep(2.0 - ((time.time() - starttime) % 2.0))
+
+
+email_body = 'Please check your system and resolve the issue as soon as possible.'
