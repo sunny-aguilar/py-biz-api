d687853cd022fe0b6e135f09310bf837b4466d0c
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-18 23:09:11 -0700

    client work

--- run.py
+++ run.py
@@ -31,7 +31,7 @@ for file in files:
 
             # add fields to item dictionary
             if desc == 1:
-                line = line[-5:-7]
+                line = line[0:4]
                 line = line.strip()
                 print(f'Line: {line}')
                 item_dict[item_fields[desc]] = line
