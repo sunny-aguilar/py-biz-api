b776cdd9258c1b63b82edcaa9f1c8a5885cde35d
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-18 19:33:41 -0700

    email

---
+++ changeImage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+#   This script implements a simple email client to prepare an email for
+#   sending. The email and SMTP modules are used as the workhorses behind the
+#   email and the OS module is used to work with the Operating System.
+#
+#
+#   Author:     Sandro Aguilar
+#   Date:       May 16, 2020
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
