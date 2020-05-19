3eb941e1b70eb0639a44ff7cf298fea675263d63
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-19 00:41:43 -0700

    client work

--- reports.py
+++ reports.py
@@ -2,6 +2,8 @@
 import reportlab
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+import datetime
 
 # report name
 report_name = 'processed.pdf'
@@ -9,6 +11,15 @@ report_name = 'processed.pdf'
 # report object
 report = SimpleDocTemplate(report_name)
 
+# get styles
+styles = getSampleStyleSheet()
 
+# get date
+date_data = datetime.datetime.now()
+date_str = date_data.strftime('%b %d, %Y')
+print(date_str)
+
+# add report title
+report_title = Paragraph('Processed Update on <Todays Date>', styles['h1'])
 
 
