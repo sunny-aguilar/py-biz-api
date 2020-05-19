915d110a0ce043c5200e51458fcab41a8039a45b
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-19 00:54:15 -0700

    client work

--- reports.py
+++ reports.py
@@ -10,6 +10,30 @@ import os
 
 # generate report
 def generate_report():
+    # directory of description files
+    directory = './supplier-data/descriptions/'
+    files = os.listdir(directory)
+
+    item_name = ''
+    item_weight = 0
+
+    # open each text file
+    for file in files:
+        with open(directory+file, 'r+') as current_file:
+            desc = 0
+            for line in current_file:
+                # add fields to item dictionary
+                if desc == 0:
+                    item_name = line.strip()
+
+                elif desc == 1:
+                    # convert weight string to integer
+                    item_weight = int(line[0:4].strip())
+
+                # go to next item in file
+                desc += 1
+
+
     # report name
     report_name = 'processed.pdf'
 
