e8e844a01a49bec50567f8f64ad99286411c74c4
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-19 00:35:19 -0700

    client work

--- reports.py
+++ reports.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+import reportlab
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+
+# report name
+report_name = 'processed.pdf'
+
+# report object
+report = SimpleDocTemplate(report_name)
 
 
 
