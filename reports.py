4c8c48ee00c707c4601d7a2292ad8830b3617915
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-29 21:47:26 -0700

    client work

--- reports.py
+++ reports.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Author:       Sandro
+# Author:       Sandro A
 # Date:
 # Project:
 #
