433447a362a1869b81300accebde0c8116ed19ff
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-01 10:36:03 -0700

    client work

--- reports.py
+++ reports.py
@@ -2,7 +2,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Author:       Sandro Aguilar
 # Date:         May 12, 2020
-# Project:      Coursera: Goog
+# Project:      Coursera: Googl
 #
 #
 #
